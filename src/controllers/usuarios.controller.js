cd32623988a8f0f9c29a975558a80e59f78e5331
Daniel Mudry <danimudry@gmail.com>
2024-05-31 14:51:17 -0300

    RUTA DE USUARIOS

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,7 +1,22 @@
 const getAllUsuarios = (req, res) => {
-  res.send("llegamos a controller");
+  res.send("llegamos a controller GET ALL USUARIO");
+};
+
+const createUsuario = (req, res) => {
+  res.send("ESTOY EN POST USUARIO");
+};
+
+const updateUsuario = (req, res) => {
+  res.send("ESTOY EN UPDATE USUARIO");
+};
+
+const deleteUsuario = (req, res) => {
+  res.send("estoy en DELETE USUARIO");
 };
 
 module.exports = {
   getAllUsuarios,
+  createUsuario,
+  deleteUsuario,
+  updateUsuario,
 };
--- src/routes/api/usuarios.js
+++ src/routes/api/usuarios.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
-const { getAllUsuarios } = require("../../controllers/usuarios.controller");
+const {
+  getAllUsuarios,
+  createUsuario,
+  deleteUsuario,
+  updateUsuario,
+} = require("../../controllers/usuarios.controller");
 
 router.get("/", getAllUsuarios);
+router.post("/", createUsuario);
+router.delete("/", deleteUsuario);
+router.put("/", updateUsuario);
 
 module.exports = router;
