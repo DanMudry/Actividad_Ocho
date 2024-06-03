a8ad16224c57344fe1c116986ab68effcb632c9e
Daniel Mudry <danimudry@gmail.com>
2024-06-03 13:39:23 -0300

    Borre comentarios

--- src/app.js
+++ src/app.js
@@ -5,12 +5,6 @@ const express = require("express");
 const app = express();
 app.use(express.json()); //traigo json para poder utilizar el req.body
 
-//middlewares
-/*app.use((req, res, next) => {
-  console.log(req.headers);
-  next();
-});*/
-
 //empiezo rutas
 app.use("/api", require("./routes/api"));
 
